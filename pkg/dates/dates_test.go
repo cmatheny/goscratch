441c953646f501b9f461976aa41eeb5e050d01fa
Chris Matheny <cmatheny@asu.edu>
2019-09-08 00:51:52 -0700

    Add some more dummy logic for testing codebuild

--- pkg/dates/dates_test.go
+++ pkg/dates/dates_test.go
@@ -6,7 +6,11 @@ import (
 )
 
 func TestGetDate(t *testing.T) {
-
+	today := GetDate()
+	expected := time.Now()
+	if expected != today {
+		t.Errorf("Date mismatch. exp: %s got: %s", expected, today)
+	}
 }
 
 func TestGetTomorrow(t *testing.T) {
